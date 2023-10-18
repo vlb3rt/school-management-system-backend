a5d4c0736a0c6c60a981c4f81331b904bdd5cd8a
Albert <a.strozynski@pixel.pl>
2023-10-18 10:57:43 +0100

    Initial Commit

---
+++ src/test/java/vlb3rt/schoolmanagment/system/SchoolManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package vlb3rt.schoolmanagment.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
